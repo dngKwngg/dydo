2fdffa3fd20f4768dbac55ea8457feb2776ac44e
VuThiThanhVinh-21020422 <21020422@vnu.edu.vn>
2024-08-20 20:18:47 +0700

    food menu ver 1.2.1

--- apps/src/screens/foodScreen.js
+++ apps/src/screens/foodScreen.js
@@ -25,39 +25,45 @@ const FoodScreen = () => {
 	}, []);
 	//Tách món ăn
 	const lauFoods = foods.filter((food) => food.type === "Lẩu Thái Tomyum");
-	const nuongFoods = foods.filter((food) => food.type === "Đồ nướng than hoa");
+	const nuongFoods = foods.filter(
+		(food) => food.type === "Đồ nướng than hoa"
+	);
 	return (
 		<div>
 			<Header label="food" />
 			<div className="food-menu">
 				<h2 className="type-food">Đồ nướng than hoa</h2>
-				{nuongFoods.map((food) => {
-					return (
-						<div key={food.id} className="food-item">
-							<div className="food-image">
-								<img src={food.src} alt="" />
+				<div className="food-category">
+					{nuongFoods.map((food) => {
+						return (
+							<div key={food.id} className="food-item ">
+								<div className="food-image">
+									<img src={food.src} alt="" />
+								</div>
+								<div className="food-info">
+									<h3>{food.item_name}</h3>
+									<p>Price: {food.price}</p>
+								</div>
 							</div>
-							<div className="food-info">
-								<h3>{food.item_name}</h3>
-								<p>Price: {food.price}</p>
-							</div>
-						</div>
-					);
-				})}
+						);
+					})}
+				</div>
 				<h2 className="type-food">Lẩu thái Tomyum</h2>
-				{lauFoods.map((food) => {
-					return (
-						<div key={food.id} className="food-item">
-							<div className="food-image">
-								<img src={food.src} alt="" />
+				<div className="food-category">
+					{lauFoods.map((food) => {
+						return (
+							<div key={food.id} className="food-item">
+								<div className="food-image">
+									<img src={food.src} alt="" />
+								</div>
+								<div className="food-info">
+									<h3>{food.item_name}</h3>
+									<p>Price: {food.price}</p>
+								</div>
 							</div>
-							<div className="food-info">
-								<h3>{food.item_name}</h3>
-								<p>Price: {food.price}</p>
-							</div>
-						</div>
-					);
-				})}
+						);
+					})}
+				</div>
 			</div>
 		</div>
 	);
