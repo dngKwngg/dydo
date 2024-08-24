da6851602117de4e98a4fd80f5b95bbc367b02bd
VuThiThanhVinh-21020422 <21020422@vnu.edu.vn>
2024-08-24 22:47:09 +0700

    add some comments

--- apps/src/components/ListContext.js
+++ apps/src/components/ListContext.js
@@ -2,7 +2,7 @@
 import React, { createContext, useState, useEffect } from "react";
 
 export const ListContext = createContext();
-
+// tạo 1 list context để lưu trữ danh sách món ăn vào localStorage
 export const ListProvider = ({ children }) => {
 	const [list, setList] = useState(() => {
 		const saveList = localStorage.getItem("list");
--- apps/src/components/counterItem.js
+++ apps/src/components/counterItem.js
@@ -4,93 +4,117 @@ import { Button } from "antd";
 import { PlusOutlined, MinusOutlined } from "@ant-design/icons";
 import { ListContext } from "./ListContext";
 const CounterItem = ({ item }) => {
-	// const [list, setList] = useState(()=>{
-	// 	const saveList = localStorage.getItem("list");
-	// 	return saveList !== null ? JSON.parse(saveList) : [];
-	// });
-	// useEffect(()=>{
-	// 	localStorage.setItem("list", JSON.stringify(list));
-	// }, [list]);
-
-	// const clearList = () => {
-	// 	setList([]); // Cập nhật state với mảng rỗng
-	// };
-
 	const { list, setList } = useContext(ListContext);
 	const increment = () => {
+		// nếu list rỗng thì thêm vào list
 		if (list.length === 0) {
+			// sao chép bản sao của list từ localStorage gán vào storeData
 			let storeData = JSON.parse(localStorage.getItem("list")) || [];
+			// thêm item vào list với số lượng là 1
 			storeData.push({ item_id: item.item_id, quantity: 1 });
+			// cập nhật storeData vào localStorage
 			setTimeout(() => {
 				localStorage.setItem("list", JSON.stringify(storeData));
 			}, 0);
+			// cập nhật list
 			setList([...storeData]);
 		} else {
+			// nếu list không rỗng
+			// tìm xem item đã có trong list chưa
 			const index = list.findIndex(
 				(element) => element.item_id === item.item_id
 			);
+			//nếu chưa có thì thêm vào list với số lượng là 1
 			if (index === -1) {
+				// sao chép bản sao của list từ localStorage gán vào storeData
 				let storeData = JSON.parse(localStorage.getItem("list")) || [];
+				// thêm item vào list với số lượng là 1
 				storeData.push({ item_id: item.item_id, quantity: 1 });
+				// cập nhật storeData vào localStorage
 				setTimeout(() => {
 					localStorage.setItem("list", JSON.stringify(storeData));
 				}, 0);
+				// cập nhật list
 				setList([...storeData]);
 			} else {
+				// nếu đã có thì tăng số lượng lên 1
+				// sao chép bản sao của list từ localStorage gán vào storeData
 				let storeData = JSON.parse(localStorage.getItem("list")) || [];
+				//tăng số lượng của item đó lên 1
 				storeData[index].quantity++;
+				// cập nhật storeData vào localStorage
 				setTimeout(() => {
 					localStorage.setItem("list", JSON.stringify(storeData));
 				}, 0);
-
+				// cập nhật list
 				setList([...storeData]);
 			}
 		}
-		// clearList();
+		
 	};
 	const decrement = () => {
+		// nếu list rỗng thì không làm gì cả
 		if (list.length === 0) {
 			return;
 		} else {
+			// nếu list không rỗng
+			// tìm xem item đã có trong list chưa
 			const index = list.findIndex(
 				(element) => element.item_id === item.item_id
 			);
+			//nếu chưa có thì không làm gì cả
 			if (index === -1) {
 				return;
 			} else {
+				// nếu đã có thì giảm số lượng đi 1
+				// sao chép bản sao của list từ localStorage gán vào storeData
 				let storeData = JSON.parse(localStorage.getItem("list")) || [];
+				// giảm số lượng của item đó đi 1
 				storeData[index].quantity--;
+				// nếu số lượng bằng 0 thì xóa item đó khỏi list
 				if (storeData[index].quantity === 0) {
 					storeData.splice(index, 1);
 				}
+				// cập nhật storeData vào localStorage
 				setTimeout(() => {
 					localStorage.setItem("list", JSON.stringify(storeData));
 				}, 0);
+				// cập nhật list
 				setList([...storeData]);
 			}
 		}
 	};
-	const printQuantity = () => {
+	// lấy số lượng của item
+	const getQuantity = () => {
+		// lấy index của item trong list
 		const index = list.findIndex((element) => element.item_id === item.item_id);
+		// nếu không tìm thấy thì không làm gì cả
 		if (index === -1) {
 			return 0;
 		} else {
+			// nếu tìm thấy thì trả về số lượng
 			return list[index].quantity;
 		}
 	};
+	// xóa item khỏi list
 	const resetItem = () => {
+		// tìm index của item trong list
 		const index = list.findIndex((element) => element.item_id === item.item_id);
+		// nếu không tìm thấy thì không làm gì cả
 		if (index === -1) {
 			return;
 		} else {
+			// nếu tìm thấy thì xóa item đó khỏi list
+			// sao chép bản sao của list từ localStorage gán vào storeData
 			let storeData = JSON.parse(localStorage.getItem("list")) || [];
+			// xóa item khỏi list
 			storeData.splice(index, 1);
+			// cập nhật storeData vào localStorage
 			setTimeout(() => {
 				localStorage.setItem("list", JSON.stringify(storeData));
 			}, 0);
-			
+			// cập nhật list
 			setList([...storeData]);
-			
 		}
 	};
 	const reset = () => {
@@ -100,7 +124,7 @@ const CounterItem = ({ item }) => {
 	return (
 		<>
 			<Button onClick={decrement} icon={<MinusOutlined />} />
-			<p>{printQuantity()}</p>
+			<p>{getQuantity()}</p>
 			<Button onClick={increment} icon={<PlusOutlined />} />
 			<Button onClick={resetItem}>Reset</Button>
 		</>
