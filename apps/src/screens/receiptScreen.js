db22ea9b154be2bca5a2da67aa4b83008eab3902
VuThiThanhVinh-21020422 <21020422@vnu.edu.vn>
2024-08-25 03:55:32 +0700

    Fix bug total receipt

--- apps/src/screens/receiptScreen.js
+++ apps/src/screens/receiptScreen.js
@@ -11,65 +11,61 @@ const ReceiptScreen = () => {
 	const [loading, setLoading] = useState(true);
 	const [total, setTotal] = useState(0);
 	const [check, setCheck] = useState(false);
-			
-useEffect(() => {
-	// Gọi API để lấy dữ liệu
-	
-	const fetchData = async () => {
-		await delay(1000);
-		try {
-			console.log(listItem);
-			const response = await fetch(
-				"http://localhost:8080/menu/listFoodById",
-				{
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({list_item: list}),
-				}
-			);
-			const data = await response.json();
-			// if (arraysAreEqual(listItem, data.data)) {
-			// 	setCheck(true);
-			// } else {
-			// 	setListItem(data.data);
-			// 	setCheck(false);
-			// }
-			setListItem(data.data);
-
-			// console.log(data.data);
-			setLoading(false);
-			
-		} catch (error) {
-			console.error("Error fetching data:", error);
-			setLoading(false);
-		}
-	};
-
-	
-
-	fetchData();
-	
-}, [list]);
 
 	useEffect(() => {
-		const getPrice =  async () => {
+		// Gọi API để lấy dữ liệu
+
+		const fetchData = async () => {
+			await delay(1000);
+			try {
+				console.log(listItem);
+				const response = await fetch(
+					"http://localhost:8080/menu/listFoodById",
+					{
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({ list_item: list }),
+					}
+				);
+				const data = await response.json();
+				// if (arraysAreEqual(listItem, data.data)) {
+				// 	setCheck(true);
+				// } else {
+				// 	setListItem(data.data);
+				// 	setCheck(false);
+				// }
+				setListItem(data.data);
+
+				// console.log(data.data);
+				setLoading(false);
+			} catch (error) {
+				console.error("Error fetching data:", error);
+				setLoading(false);
+			}
+		};
+
+		fetchData();
+	}, [list]);
+
+	useEffect(() => {
+		const getPrice = async () => {
 			let current_price = 0;
 			if (listItem == undefined) {
 				return;
 			}
 			if (listItem.length > 0) {
-				listItem.forEach( async (item) => {
-					list.forEach( async (element) => {
-						if(item.item_id === element.item_id) {
+				listItem.forEach(async (item) => {
+					list.forEach(async (element) => {
+						if (item.item_id === element.item_id) {
 							current_price += item.price * element.quantity;
 						}
-					})
-				})
+					});
+				});
 			}
 			setTotal(current_price);
-		}
+		};
 		console.log("listItem", listItem);
 		getPrice();
 	}, [list, []]);
@@ -85,21 +81,22 @@ useEffect(() => {
 							<h2>Không có món ăn nào được chọn</h2>
 						</div>
 					) : (
-						
 						listItem.map((item) => {
-							
 							return (
-									<div className="receipt">
-										<MenuItem item={item} />
-									</div>
+								<div className="receipt">
+									<MenuItem item={item} />
+								</div>
 							);
 						})
-						
 					)}
-					<h2>Tiền: {total}</h2>
+					<div className="total">
+						<h2>
+							Tổng số tiền:{" "}
+							{new Intl.NumberFormat("vi-VN").format(total)}
+						</h2>
+					</div>
 				</div>
-			  )} 
-			
+			)}
 		</div>
 	);
 };
